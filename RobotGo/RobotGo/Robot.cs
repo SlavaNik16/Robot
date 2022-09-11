5cac1ce688876d05c29ef654dbb6eb733d95f675
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-09-11 23:32:35 +0300

    Add files via upload

---
+++ RobotGo/RobotGo/Robot.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+
+namespace RobotGo
+{
+    class Robot
+    {
+        public int x = Console.WindowWidth / 2;
+        public int y = Console.WindowHeight / 2;
+        public int count = 0;
+
+        public void Shag(ref ConsoleKeyInfo key, ref int x, ref int y)
+        {
+
+            if (key.KeyChar == 119 || key.KeyChar == 87) 
+            { 
+                Console.SetCursorPosition(x, --y);
+            }
+            else if (key.KeyChar == 115 || key.KeyChar == 83)
+            {
+                Console.SetCursorPosition(x, ++y);
+            }
+            else if (key.KeyChar == 97 || key.KeyChar == 65)
+            {
+                Console.SetCursorPosition(--x, y);
+            }
+            else if (key.KeyChar == 100 || key.KeyChar == 68)
+            {
+                Console.SetCursorPosition(++x, y);
+            }
+                
+                count++;
+
+        }
+
+        public bool End()
+        {
+            if ((x == (Console.WindowWidth / 2) && y == (Console.WindowHeight / 2) || count == 20))
+            {
+                if (count == 20)
+                {
+                    Console.WriteLine("Нет пути домой!!!");
+                }
+                return false;
+            }
+             return true;
+
+            
+        }
+    }
+}
