36915678d3cace94e9ae657aed0021dd5b33f37e
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-09-12 15:37:58 +0300

    Add files via upload

---
+++ Robotgo/Robotgo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Robotgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            Robot rob = new Robot();
+            int i = 0;
+
+            Console.WriteLine("Введите путь робота через клавиатуру (W - вверх, S - вниз, A - влево, D - вправо)\nЦель игры вернутся на место прибытия.");
+            Console.WriteLine("У вас максимум 20 ходов включительно!");
+            Console.WriteLine($"Ваши начальные координаты x = {rob.x}, y = {rob.y}");
+            Console.SetCursorPosition(rob.x, rob.y);
+            Console.CursorVisible = false;
+            ConsoleKeyInfo key;
+            Console.Write((char)(127));
+
+            while (run)
+            {
+
+                key = Console.ReadKey(true);
+                rob.Shag(ref key, ref rob.x, ref rob.y);
+                run = rob.End();
+                i++;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Ваши конечные координаты x = {rob.x}, y = {rob.y}");
+            Console.WriteLine($"Всего шагов: {rob.count}");
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ Robotgo/Robotgo/Robot.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace Robotgo
+{
+    class Robot
+    {
+        public int x = Console.WindowWidth / 2;
+        public int y = Console.WindowHeight / 2;
+        public int count = 0;
+
+        public void Shag(ref ConsoleKeyInfo key, ref int x, ref int y)
+        {
+
+            if (key.KeyChar == 119 || key.KeyChar == 87)
+            {
+                Console.SetCursorPosition(x, --y);
+                Console.Write((char)(30));
+            }
+            else if (key.KeyChar == 115 || key.KeyChar == 83)
+            {
+                Console.SetCursorPosition(x, ++y);
+                Console.Write((char)(31));
+            }
+            else if (key.KeyChar == 97 || key.KeyChar == 65)
+            {
+                Console.SetCursorPosition(--x, y);
+                Console.Write((char)(17));
+            }
+            else if (key.KeyChar == 100 || key.KeyChar == 68)
+            {
+                Console.SetCursorPosition(++x, y);
+                Console.Write((char)(16));
+            }
+
+            count++;
+
+        }
+
+        public bool End()
+        {
+            if ((x == (Console.WindowWidth / 2) && y == (Console.WindowHeight / 2)) || count > 20) 
+            {
+                if (count > 20)
+                {
+                    Console.WriteLine("Нет пути домой!!!");
+                }
+                return false;
+            }
+            return true;
+
+
+        }
+    }
+}
